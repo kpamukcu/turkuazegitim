105118c4cf9840419c64ccf76b35f0503d57eba7
kpamukcu <kapawebtasarim@gmail.com>
2023-11-06 15:37:52 +0300

    Kök Dizin Yapısı Oluşturuldu

---
+++ sablon.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Css Files -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+
+    <button class="btn btn-info">Deneme</button>
+
+
+
+    <script src="js/jquery.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
