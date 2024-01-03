6e395b1f617282b79a35b969d7a3ed44ad649a94
kpamukcu <kapawebtasarim@gmail.com>
2024-01-03 13:40:28 +0300

    Eklemeler

--- header.php
+++ header.php
@@ -23,19 +23,19 @@ require_once('baglan.php');
     <!-- Header Section Start -->
     <header id="header" class="bg-light">
         <section id="menu">
-            <div class="container">
+            <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
                         <nav class="navbar navbar-expand-lg navbar-light">
-                            <a class="navbar-brand" href="index.php">TURKUAZ EĞİTİM</a>
+                            <!-- <a class="navbar-brand" href="index.php">TURKUAZ EĞİTİM</a> -->
                             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                                 <span class="navbar-toggler-icon"></span>
                             </button>
                             <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                                <ul class="navbar-nav ml-auto">
-                                    <li class="nav-item active">
+                                <ul class="navbar-nav m-auto">
+                                    <!-- <li class="nav-item active">
                                         <a class="nav-link" href="index.php">Ana Sayfa</a>
-                                    </li>
+                                    </li> -->
                                     <li class="nav-item dropdown">
                                         <a class="nav-link dropdown-toggle" href="" role="button" data-toggle="dropdown" aria-expanded="false">
                                             Dil Eğitimi
@@ -74,9 +74,63 @@ require_once('baglan.php');
                                             ?>
                                         </div>
                                     </li>
-                                    <li class="nav-item ml-5">
-                                        <a class="nav-link btn btn-warning" href="#">Bize Ulaşın</a>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="" role="button" data-toggle="dropdown" aria-expanded="false">
+                                            Yurt Dışı Eğitim
+                                        </a>
+                                        <div class="dropdown-menu">
+                                            <?php
+                                            $menu3 = $db->prepare('select * from egitimler where kategori="Yurt Dışı Eğitim" order by egitimAdi asc ');
+                                            $menu3->execute();
+
+                                            if ($menu1->rowCount()) {
+                                                foreach ($menu3 as $menu3Satir) {
+                                            ?>
+                                                    <a class="dropdown-item" href="egitimler.php?egitimID=<?php echo $menu3Satir['id']; ?>"><?php echo $menu3Satir['egitimAdi']; ?></a>
+                                            <?php
+                                                }
+                                            }
+                                            ?>
+                                        </div>
                                     </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="" role="button" data-toggle="dropdown" aria-expanded="false">
+                                            Turkuaz Akademi
+                                        </a>
+                                        <div class="dropdown-menu">
+                                            <?php
+                                            $menu4 = $db->prepare('select * from egitimler where kategori="Turkuaz Akademi" order by egitimAdi asc ');
+                                            $menu4->execute();
+
+                                            if ($menu1->rowCount()) {
+                                                foreach ($menu4 as $menu4Satir) {
+                                            ?>
+                                                    <a class="dropdown-item" href="egitimler.php?egitimID=<?php echo $menu4Satir['id']; ?>"><?php echo $menu4Satir['egitimAdi']; ?></a>
+                                            <?php
+                                                }
+                                            }
+                                            ?>
+                                        </div>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="egitimler.php?egitimID=XXXXX">Oxford Üniversitesi Sınav Merkezi</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="egitimler.php?egitimID=XXXXX">Turkuaz Dil Eğitim Derneği</a>
+                                    </li>
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="" role="button" data-toggle="dropdown" aria-expanded="false">
+                                            Hakkımızda
+                                        </a>
+                                        <div class="dropdown-menu">
+                                            <a class="dropdown-item" href="">Sub Menü 1</a>
+                                            <a class="dropdown-item" href="">Sub Menü 2</a>
+                                            <a class="dropdown-item" href="">Sub Menü 3</a>
+                                        </div>
+                                    </li>
+                                    <!-- <li class="nav-item ml-5">
+                                        <a class="nav-link btn btn-warning" href="#">Bize Ulaşın</a>
+                                    </li> -->
                                 </ul>
                             </div>
                         </nav>
