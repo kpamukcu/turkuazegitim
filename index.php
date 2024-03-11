e607f44a943f7b8e9c1b92bb4366a46ab18d9d62
kpamukcu <kapawebtasarim@gmail.com>
2024-03-11 13:31:27 +0300

    Ana Sayfa About Us Section Oluşturulmaya Başlandı

--- index.php
+++ index.php
@@ -1,3 +1,67 @@
 <?php require_once('header.php'); ?>
 <?php require_once('slider.php'); ?>
+
+<!-- About Us Section Start -->
+<section id="aboutUs" class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ullam modi culpa, sunt vel excepturi! Quod culpa porro neque quis molestias numquam sunt quidem nam veritatis alias blanditiis commodi, delectus dolorem, accusamus saepe, quia obcaecati recusandae. Nemo obcaecati maxime, saepe soluta, porro ipsam omnis necessitatibus deleniti quia sapiente enim perferendis corporis neque sit pariatur tempore nobis quis suscipit, beatae quisquam officiis! Dolores voluptate aliquid nesciunt corporis.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste ullam modi culpa, sunt vel excepturi! Quod culpa porro neque quis molestias numquam sunt quidem nam veritatis alias blanditiis commodi, delectus dolorem, accusamus saepe, quia obcaecati recusandae. Nemo obcaecati maxime, saepe soluta, porro ipsam omnis necessitatibus deleniti quia sapiente enim perferendis corporis neque sit pariatur tempore nobis quis suscipit, beatae quisquam officiis! Dolores voluptate aliquid nesciunt corporis.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <a href="hakkimizda.php">
+                    <div class="card border-0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-3 my-auto">
+                                    <i class="bi bi-bullseye"></i>
+                                </div>
+                                <div class="col-9 my-auto">
+                                    <span class="tanitimBaslik">Niçin Turkuaz Dil Eğitim?</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="tanitim-dersi.php">
+                    <div class="card border-0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-3 my-auto">
+                                    <i class="bi bi-play-circle"></i>
+                                </div>
+                                <div class="col-9 my-auto">
+                                    <span class="tanitimBaslik">Ücretsiz Tanıtım Dersi</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4">
+                <a href="uzaktan-egitim.php">
+                    <div class="card border-0">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-3 my-auto">
+                                    <i class="bi bi-laptop"></i>
+                                </div>
+                                <div class="col-9 my-auto">
+                                    <span class="tanitimBaslik">Uzaktan Eğitimde Turkuaz</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- About Us Section End -->
+
 <?php require_once('footer.php'); ?>
