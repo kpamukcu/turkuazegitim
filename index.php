cd55469b11d8008f158263a2cc3c86ae056f0308
kpamukcu <kapawebtasarim@gmail.com>
2024-03-18 16:27:12 +0300

    index.php üzerinde yeni section'lar oluşturuldu

--- index.php
+++ index.php
@@ -70,16 +70,213 @@
 
 
 <!-- Sizden Gelenler Section Start -->
-<section id="sizdenGelenler">
-    <div class="container">
+<section id="sizdenGelenler" class="py-5">
+    <div class="container-fluid">
         <div class="row">
-            <div class="col-12">
-                
+            <div class="col-12 text-center">
+                <h3>Sizden Gelenler</h3>
+                <p>Turkuaz Eğitimin Öğrencilerinden Gelen Yorumlar</p>
+            </div>
+        </div>
+        <div class="row mt-4 mb-5">
+            <div class="col-md-4">
+                <div class="card border-0">
+                    <div class="card-header border-bottom-0">
+                        Turkuaz Yabancı Dil Kursu’nda iki yıl eğitim aldım. Bu sürede kaliteli bir eğitim aldığımı söyleyebilirim. Kursta tecrübeli hocaların danışmanlığında sınava yönelik İngilizce eğitimi veriliyor. İlk gördüğüm KPDS’de 47,5 puan aldım.
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-2">
+                                <img src="img/Habibe-Bilgili-1-100x100.png" alt="Habibe Bilgili" style="width: 65px;">
+                            </div>
+                            <div class="col-10 my-auto">
+                                <h5 class="m-0">Habibe Bilgili</h5>
+                                <small>Öğretim Üyesi</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0">
+                    <div class="card-header border-bottom-0">
+                        Turkuaz Yabancı Dil Kursu’nda iki yıl eğitim aldım. Bu sürede kaliteli bir eğitim aldığımı söyleyebilirim. Kursta tecrübeli hocaların danışmanlığında sınava yönelik İngilizce eğitimi veriliyor. İlk gördüğüm KPDS’de 47,5 puan aldım.
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-2">
+                                <img src="img/Habibe-Bilgili-1-100x100.png" alt="Habibe Bilgili" style="width: 65px;">
+                            </div>
+                            <div class="col-10 my-auto">
+                                <h5 class="m-0">Habibe Bilgili</h5>
+                                <small>Öğretim Üyesi</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0">
+                    <div class="card-header border-bottom-0">
+                        Turkuaz Yabancı Dil Kursu’nda iki yıl eğitim aldım. Bu sürede kaliteli bir eğitim aldığımı söyleyebilirim. Kursta tecrübeli hocaların danışmanlığında sınava yönelik İngilizce eğitimi veriliyor. İlk gördüğüm KPDS’de 47,5 puan aldım.
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-2">
+                                <img src="img/Habibe-Bilgili-1-100x100.png" alt="Habibe Bilgili" style="width: 65px;">
+                            </div>
+                            <div class="col-10 my-auto">
+                                <h5 class="m-0">Habibe Bilgili</h5>
+                                <small>Öğretim Üyesi</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <a href="yorumlar.php" class="btn btn-lacivert">Tüm Öğrenci Yorumlarını Okuyun</a>
             </div>
         </div>
     </div>
 </section>
 <!-- Sizden Gelenler Section End -->
+
+<!-- Yurtdışı Eğitim Programları Start -->
+<section id="yurtDisiProgramlar" class="py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h3>YURT DIŞI EĞİTİM PROGRAMLARI</h3>
+                <p>Turkuaz Eğitim Olarak Sunduğumuz Programlar ve Hizmetler</p>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <?php
+            $yurtDisiEgitim = $db->prepare('select * from egitimler where kategori="Yurt Dışı Eğitim" order by id desc');
+            $yurtDisiEgitim->execute();
+
+            if ($yurtDisiEgitim->rowCount()) {
+                foreach ($yurtDisiEgitim as $yurtDisiEgitimSatir) {
+            ?>
+                    <div class="col-md-3">
+                        <a href="egitimler.php?egitimID=<?php echo $yurtDisiEgitimSatir['id']; ?>">
+                            <div class="card">
+                                <img src="<?php echo substr($yurtDisiEgitimSatir['gorsel'], 3); ?>" alt="<?php echo $yurtDisiEgitimSatir['egitimAdi']; ?>">
+                                <div class="card-footer bg-mavi cardFooter">
+                                    <?php echo $yurtDisiEgitimSatir['egitimAdi']; ?>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+            <?php
+                }
+            }
+            ?>
+        </div>
+    </div>
+</section>
+<!-- Yurtdışı Eğitim Programları End -->
+
+<!-- Dil Eğitimi Programları Start -->
+<section id="dilEgitimleri" class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h3>TURKUAZ EĞİTİM DİL PROGRAMLARI</h3>
+                <p>Turkuaz Eğitim Olarak Gerçekleştirdiğimiz Dil Eğitimleri</p>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <?php
+            $dilEgitimi = $db->prepare('select * from egitimler where kategori="Dil Eğitimi" order by id desc');
+            $dilEgitimi->execute();
+
+            if ($dilEgitimi->rowCount()) {
+                foreach ($dilEgitimi as $dilEgitimiSatir) {
+            ?>
+                    <div class="col-md-3">
+                        <a href="egitimler.php?egitimID=<?php echo $dilEgitimiSatir['id']; ?>">
+                            <div class="card">
+                                <img src="<?php echo substr($dilEgitimiSatir['gorsel'], 3); ?>" alt="<?php echo $dilEgitimiSatir['egitimAdi']; ?>">
+                                <div class="card-footer bg-mavi cardFooter">
+                                    <?php echo $dilEgitimiSatir['egitimAdi']; ?>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+            <?php
+                }
+            }
+            ?>
+        </div>
+    </div>
+</section>
+<!-- Dil Eğitimi Programları End -->
+
+<!-- Turkuaz Akademi Start -->
+<section id="turkuazAkademi" class="py-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h3>TURKUAZ AKADEMİ PROGRAMLARI</h3>
+                <p>Turkuaz Akademi Olarak Öğrencilerimizin Okul Derslerine Yardımcı Oluyoruz.</p>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-3">
+                Turkuaz Akademi İçerik
+            </div>
+            <div class="col-md-3">
+                Turkuaz Akademi İçerik
+            </div>
+            <div class="col-md-3">
+                Turkuaz Akademi İçerik
+            </div>
+            <div class="col-md-3">
+                Turkuaz Akademi İçerik
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Turkuaz Akademi End -->
+
+<!-- Turkuaz Akademi Ağ Günlüğü Start -->
+<section id="agGunlugu" class="py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 text-center">
+                <h2>TURKUAZ EĞİTİM AĞ GÜNLÜĞÜ (BLOG)</h2>
+                <p>Dil Eğitimi, Yurt Dışı Programları vvb Güncel Blog Yazılarımız ve Makalelerimizi Takip Edebilirsiniz</p>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <?php
+            $agGunlugu = $db->prepare('select * from yazilar order by id desc');
+            $agGunlugu->execute();
+
+            if ($agGunlugu->rowCount()) {
+                foreach ($agGunlugu as $agGunluguSatir) {
+            ?>
+                    <div class="col-md-4">
+                        <a href="makale.php?postID=<?php echo $agGunluguSatir['id']; ?>">
+                            <div class="card shadow">
+                                <img src="<?php echo substr($agGunluguSatir['gorsel'], 3); ?>" alt="">
+                                <div class="card-footer text-center">
+                                    <h3 style="font-size: 14px; color:#393939;"><?php echo $agGunluguSatir['baslik']; ?></h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+            <?php
+                }
+            }
+            ?>
+        </div>
+    </div>
+</section>
+<!-- Turkuaz Akademi Ağ Günlüğü End -->
 
 
 
